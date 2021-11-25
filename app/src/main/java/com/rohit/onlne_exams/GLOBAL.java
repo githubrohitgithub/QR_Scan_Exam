4a65c3cca0d656268ba4b0433cd34120ca966248
CC4 <rpachori21@gmail.com>
2021-11-25 13:32:46 +0530

    first

---
+++ app/src/main/java/com/rohit/onlne_exams/GLOBAL.java
@@ -0,0 +1,11 @@
+package com.rohit.onlne_exams;
+
+import java.util.ArrayList;
+
+public class GLOBAL {
+
+   public static String QUESTION_SET_CONSTANT="A";
+   public  static  ArrayList<String> SUBJECT_CODE_ARRAYLIST = new ArrayList<>();
+
+
+}
