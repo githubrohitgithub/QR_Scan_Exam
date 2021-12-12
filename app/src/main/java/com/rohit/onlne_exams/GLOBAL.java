47b7de499f1de27bd5f12387d31c829e407a3643
CC4 <rpachori21@gmail.com>
2021-12-12 19:49:32 +0530

    subject name & code added

--- app/src/main/java/com/rohit/onlne_exams/GLOBAL.java
+++ app/src/main/java/com/rohit/onlne_exams/GLOBAL.java
@@ -1,11 +1,15 @@
 package com.rohit.onlne_exams;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class GLOBAL {
 
    public static String QUESTION_SET_CONSTANT="ABCD"; //ABCD
    public  static  ArrayList<String> SUBJECT_CODE_ARRAYLIST = new ArrayList<>();
+
+   public  static HashMap<String, String> data = new LinkedHashMap<>();
 
 
 }
